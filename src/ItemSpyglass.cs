a4edeaa4c217219b9960875c86b141361c6b5dde
Fuami <Fuami.Cake@gmail.com>
2022-01-14 01:14:55 -0600

    Implement GroundStorable for spyglasses. ( Closes #1 ) Add Accurate WorldInteration Help. Bump version.

--- src/ItemSpyglass.cs
+++ src/ItemSpyglass.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Util;
 
 namespace spyglass.src
 {
@@ -16,16 +17,35 @@ namespace spyglass.src
             return "spyglassing";
         }
 
+        public override WorldInteraction[] GetHeldInteractionHelp( ItemSlot inSlot )
+        {
+            WorldInteraction worldInteraction = new WorldInteraction();
+            worldInteraction.ActionLangCode = "spyglass:item-spyglass-use";
+            worldInteraction.MouseButton = EnumMouseButton.Right;
+
+            return new WorldInteraction[] { worldInteraction }.Append(base.GetHeldInteractionHelp(inSlot));
+        }
+
         public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
         {
-            handling = EnumHandHandling.Handled;
-            updateState(true, byEntity);
+            if (!byEntity.Controls.Sneak)
+            {
+                handling = EnumHandHandling.Handled;
+                updateState(true, byEntity);
+            }
+            else
+                base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent,ref handling);
         }
 
         public override bool OnHeldInteractStep(float secondsUsed, ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel)
         {
-            updateState(true, byEntity);
-            return true;
+            if (!byEntity.Controls.Sneak)
+            {
+                updateState(true, byEntity);
+                return true;
+            }
+            else
+               return base.OnHeldInteractStep(secondsUsed, slot, byEntity, blockSel, entitySel);
         }
 
         public override void OnHeldInteractStop(float secondsUsed, ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel)
